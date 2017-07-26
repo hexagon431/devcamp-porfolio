2130bf53aab0c699e054d662617c7c6a8d001b8e
hexagon431 <crusader.archer@gmail.com>
2017-07-26 15:35:30 +0000

    i tried so hard to fix my sortable error but i have been forsaken by the rails gods -- Implemented initial file uploading system via carrierwave for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,74 +6,74 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.by_position
   end
-  
+
   def sort
-  params[:order].each do |key, value|
-    Portfolio.find(value[:id]).update(position: value[:position])
-  end
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
 
     render nothing: true
   end
-  
+
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build}
+    3.times { @portfolio_item.technologies.build }
   end
-  
+
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Portfolio created.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to portfolios_path, notice: 'Portfolio updated.' }
-        format.json { render :show, status: :ok, location: @blog }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
-  
+
   def show
   end
-  
+
   def destroy
     # Destroy/delete the record
     @portfolio_item.destroy
-    
+
     # Redirect
     respond_to do |format|
-      format.html { redirect_to portfolios_url, notice: 'Portfolio deleted.' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-  
+
   private
-  
+
   def portfolio_params
-    params.require(:portfolio).permit(:title, 
+    params.require(:portfolio).permit(:title,
                                       :subtitle,
                                       :body,
+                                      :main_image,
+                                      :thumb_image,
                                       technologies_attributes: [:name]
-                                      )
+                                     )
   end
-  
+
   def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
   end
